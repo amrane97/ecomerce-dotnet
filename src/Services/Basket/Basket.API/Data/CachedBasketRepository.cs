cb8f83105b9a6060230e7a72b02e00d4da2a57ea
DJEBARRI Amrane <a.djebarri@wedr.fr>
2025-09-26 14:19:26 +0200

    add redis to basket service

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data;
+
+public class CachedBasketRepository(IBasketRepository _repository, IDistributedCache cache) : IBasketRepository
+{
+    public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        var cacheBasket = await cache.GetStringAsync(userName, cancellationToken);
+
+        if (!string.IsNullOrEmpty(cacheBasket))
+        {
+            return JsonSerializer.Deserialize<ShoppingCart>(cacheBasket);
+        }
+
+        var basket = await _repository.GetBasket(userName, cancellationToken);
+        await cache.SetStringAsync(userName, JsonSerializer.Serialize(basket), cancellationToken);
+        return basket;
+    }
+
+    public async Task<ShoppingCart> StoreBasket(ShoppingCart cart, CancellationToken cancellationToken = default)
+    {
+        await _repository.StoreBasket(cart, cancellationToken);
+
+        await cache.SetStringAsync(cart.UserName, JsonSerializer.Serialize(cart), cancellationToken);
+
+        return cart;
+    }
+    public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {   
+        await _repository.DeleteBasket(userName, cancellationToken);
+
+        await cache.RemoveAsync(userName, cancellationToken);
+
+        return true;
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,7 @@
 using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Caching.Distributed;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,12 +22,35 @@ builder.Services.AddMarten(opts =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+/* on utilisera Decorate de scrutor qui est simple et plus elegant pour declarer 2 classes a une interface
+builder.Services.AddScoped<IBasketRepository>(provider =>
+{
+    var basketRepository = provider.GetRequiredService<IBasketRepository>();
+    return new CachedBasketRepository(basketRepository, provider.GetRequiredService<IDistributedCache>());
+});
+*/
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
 
 // http pipeline
 app.MapCarter();
 app.UseExceptionHandler(opts => { });
+app.UseHealthChecks("/health", 
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
